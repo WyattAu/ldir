ab1be4db3949225d21254482d05567008bce0a7d
WyattAu <WyattAu@users.noreply.github.com>
2026-05-30 06:22:48 +0000

    deploy: 795ba1655a885c68a03d96f1a6340b926ee13b52

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4cb73b8a.js',
+                path_to_root + 'searchindex-47d5b1bf.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
